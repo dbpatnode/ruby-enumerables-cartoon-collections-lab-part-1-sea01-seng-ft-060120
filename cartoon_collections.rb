6d9098f1953fd59ec3a70fc4ba277f34e55c67d5
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-14 19:34:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+array.each do |charachter|
+  puts "Hello #{charachter.capitalize}!"
+end
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+array.each_with_index do |charachter, index|
+ puts "#{index + 1} #{charachter.capitalize}"
+end
 end
